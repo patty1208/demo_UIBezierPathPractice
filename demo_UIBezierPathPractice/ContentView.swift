8320708c3c63c0c4d94c21c5d570fc09b824ece1
patty <patty99491208@gmail.com>
2021-01-19 13:12:41 +0800

    Initial Commit

---
+++ demo_UIBezierPathPractice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  demo_UIBezierPathPractice
+//
+//  Created by 林佩柔 on 2021/1/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
